6262fa32ec2a316f1fbdc4dbfeebbdfcfd18fe85
akac12z <mrjark7@gmail.com>
2025-12-24 07:28:32 +0100

    creating pagination functionality

--- withVite/src/App.jsx
+++ withVite/src/App.jsx
@@ -5,15 +5,24 @@ import Footer from "./components/Footer";
 import JobListing from "./components/JobListing";
 
 function App() {
+	const handlePageChange = (page) => {
+		console.log("Page changed to:", page);
+	};
 	return (
 		<>
 			<Header />
 			<main>
 				<JobSearch />
 
-				<JobListing />
+				<section>
+					<JobListing />
 
-				<Pagination />
+					<Pagination
+						currentPage={1}
+						totalPages={5}
+						onChange={handlePageChange}
+					/>
+				</section>
 			</main>
 
 			<Footer />
--- withVite/src/components/Pagination.jsx
+++ withVite/src/components/Pagination.jsx
@@ -1,10 +1,49 @@
-function Pagination({ currentPage, totalPages }) {
+function Pagination({ currentPage = 1, totalPages = 6, onPageChange }) {
 	// generar un array de páginas a mostrar
 	const pages = Array.from({ length: totalPages }, (_, index) => index + 1); // el Array.from da un array con el length que le pasamos sea estricto o dinámico y tiene un segundo parámetro donde está el indice y como quieres que se incilialice es + 1 para que no empiece en 0 en la paginación no tendría sentido
 
+	const isFirstPage = currentPage === 1;
+	const isLastPage = currentPage === totalPages;
+
+	const styleLeftPagination = {
+		opacity: isFirstPage ? 0.5 : 1,
+		pointerEvents: isFirstPage ? "none" : "auto",
+	};
+
+	const styleRightPagination = {
+		opacity: isLastPage ? 0.5 : 1,
+		pointerEvents: isLastPage ? "none" : "auto",
+	};
+
+	const handlePrevClick = (e) => {
+		// si le das a la flechita para atrás, vas una pagina menos
+		e.preventDefault();
+		if (!isFirstPage) {
+			onPageChange(currentPage - 1);
+		}
+	};
+	const handleNextClick = (e) => {
+		// si le das a la flechita para adelante, vas una pagina más
+		e.preventDefault();
+		if (!isLastPage) {
+			onPageChange(currentPage + 1);
+		}
+	};
+	const handleChangePage = (e, page) => {
+		e.preventDefault();
+		if (page !== currentPage) {
+			onPageChange(page);
+		}
+	};
+
 	return (
 		<nav className="pagination">
-			<a href="#">
+			<a
+				href="#"
+				onClick={handlePrevClick}
+				style={styleLeftPagination}
+				className="arrowPagination arrowLeftPagination"
+			>
 				<svg
 					width="16"
 					height="16"
@@ -23,17 +62,27 @@ function Pagination({ currentPage, totalPages }) {
 					<path d="M15 6l-6 6l6 6" />
 				</svg>
 			</a>
+
+			{pages.map((page) => {
+				return (
+					<a
+						href="#"
+						onClick={(e) => handleChangePage(e, page)}
+						className={currentPage === page ? "isActive" : ""}
+					>
+						{page}
+					</a>
+				);
+			})}
+
+			{/* botón de siguiente página */}
+
 			<a
-				className="isActive"
 				href="#"
+				className="arrowPagination arrowRightPagination"
+				style={styleRightPagination}
+				onClick={handleNextClick}
 			>
-				1
-			</a>
-			<a href="#">2</a>
-			<a href="#">3</a>
-			<a href="#">4</a>
-			<a href="#">5</a>
-			<a href="#">
 				<svg
 					width="16"
 					height="16"
