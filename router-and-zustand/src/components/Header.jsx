cf06d0a75b93c4e0e412658861f3de4164b71f35
akac12z <mrjark7@gmail.com>
2026-01-16 06:11:18 +0100

    enhanced svg

--- router-and-zustand/src/components/Header.jsx
+++ router-and-zustand/src/components/Header.jsx
@@ -8,16 +8,15 @@ function Header() {
 			<Link href="/">
 				<h1>
 					<svg
+						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						stroke="currentColor"
 						strokeLinecap="round"
 						strokeLinejoin="round"
 						strokeWidth="2"
 						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
 					>
-						<polyline points="16 18 22 12 16 6"></polyline>
-						<polyline points="8 6 2 12 8 18"></polyline>
+						<path d="m16 18 6-6-6-6M8 6l-6 6 6 6" />
 					</svg>
 					DevJobs
 				</h1>
