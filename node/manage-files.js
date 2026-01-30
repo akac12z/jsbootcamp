047a3ab67edde716f8e639507ddc5b6738b84ad2
akac12z <mrjark7@gmail.com>
2026-01-30 06:25:01 +0100

    created path with modules

--- node/manage-files.js
+++ node/manage-files.js
@@ -1,15 +1,40 @@
 import { mkdir, readFile, writeFile } from "node:fs/promises";
+// para manejar rutas no es correcto usar el / porque no funciona en todos los sistemas operativos (concretamente en windows...) para solucionarlo lo que tinee node es una libreria llamada join que viene de path lo que te permite es concatenar rutas de forma correcta y automática sin poner "/" o "\"
+import { join, basename, extname } from "node:path";
 
 const content = await readFile( './archivo.txt', 'utf-8' );
-// al readFile le pasamos la ruta que quieres que lea, el archivo.txt y el encoding utf-8 para que entienda los símbolos
-console.log( content );
+// al readFile le pasamos la ruta que quieres que lea, el archivo.txt y el encoding utf-8 para que entienda los símbolos y no lo lea como un buffer binario
+// console.log( content );
 
-const outputDir = 'output/documents'; // la ruta que quiero crear
-await mkdir( outputDir, { recursive: true } ); // el recursive es para que cree las carpetas si no exsisten de manera recursiva
+
+/*
+esta forma no funciona porque no tiene el recursive = true
+
+const outputDir2 = 'output2/documents2'; // la ruta que quiero crear
+await mkdir( outputDir2, { recursive: false } ); // el recursive es para que cree las carpetas anidadas
+await writeFile( `${ outputDir2 }/archivo-uppercase.txt`, uppercaseContent ); // este archivo se crea en la ruta que le indico con el outputDir -> esto no se podría hacer pq no tiene el recursive = true por ende, lo que hace es que no crea la carpeta output2 y da error
+*/
+
+
+// const outputDir = 'output/documents'; // la ruta que quiero crear -> forma mal hecha
+const outputDir = join( 'output', 'documents', 'new-folder' ); // la ruta que quiero crear -> forma correcta
+await mkdir( outputDir, { recursive: true } ); // el recursive es para que cree las carpetas anidadas
 
 // ahora voy a crear un archivo de escritura
 const lowercaseContent = content.toLocaleLowerCase();
-await writeFile( './archivo-uppercase.txt', lowercaseContent ); // creo el nuevo archivo con el writefile pq si hago sobre el archivo que ya tengo lo sobreescribiría
-await writeFile( `${ outputDir }/archivo-uppercase.txt`, lowercaseContent ); // este archivo se crea en la ruta que le indico con el outputDir
+const uppercaseContent = content.toLocaleUpperCase();
+await writeFile( 'archivo-uppercase.txt', lowercaseContent ); // creo el nuevo archivo con el writefile pq si hago sobre el archivo que ya tengo lo sobreescribiría
+
+// aquí tb usarías ese join para que sepa donde crearlo
+const outputFilePath = join( outputDir, 'archivo-uppercase.txt' );
+await writeFile( outputFilePath, uppercaseContent ); // este archivo se crea en la ruta que le indico con el outputDir y de manera dinámica crea la carpeta si no existe con el / o \ pertinente al sistema operativo
+// await writeFile( `${ outputDir }/archivo-uppercase.txt`, uppercaseContent ); // este archivo se crea en la ruta que le indico con el outputDir
 console.log( 'Archivo reescrito' );
 
+// para recuperar el nombre del archivo sin la extensión
+const fileName = basename( outputFilePath );
+console.log( fileName );
+
+// para recuperar la extensión del archivo
+const fileExtension = extname( outputFilePath );
+console.log( fileExtension );
